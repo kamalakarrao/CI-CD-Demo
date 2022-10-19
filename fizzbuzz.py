c84e59f1bcdf73895bfd219ab05b8399b5c3a509
Kamal <kamalakar.sambaraj@gmail.com>
2022-10-19 22:27:39 +0530

    Reformatted

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,4 @@
 """Function to solve the fizzbuzz problem."""
-
-
 def fizz_buzz(num: int) -> str:
     """This is my great and neat function to solve the famous
     Fizz Buzz problem.
@@ -10,7 +8,7 @@ def fizz_buzz(num: int) -> str:
     if num % 15 == 0:
         return "fizzbuzz"
     if num % 5 == 0:
-        return "buzz "
+        return "buzz"
     if num % 3 == 0:
         return "fizz"
     return str(num)
