a4dc019ff262a017d662231cae0f187b7161c950
Kamal <kamalakar.sambaraj@gmail.com>
2022-10-19 23:24:16 +0530

    Changed code to fail test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,10 +7,10 @@ def fizz_buzz(num: int) -> str:
     :param num: That's the number which we want the answer for
     :return: fizz, buzz, fizzbuzz or the number itself
     """
-    if num % 15 == 0:
+    if num % 3 == 0:
         return "fizzbuzz"
     if num % 5 == 0:
         return "buzz"
-    if num % 3 == 0:
+    if num % 15 == 0:
         return "fizz"
     return str(num)
