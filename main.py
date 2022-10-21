57907040bf8436495b6b3e8e670444ca7ed48a1e
Kamal <kamalakar.sambaraj@gmail.com>
2022-10-21 09:21:53 +0530

    Modified Response to text

--- main.py
+++ main.py
@@ -4,33 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-SAMPLE_RESPONSE = """
-<!DOCTYPE html>
-<title>Text Example</title>
-<style>
-  div.container {
-    background-color: #ffffff;
-  }
-
-  div.container p {
-    font-family: Arial;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: normal;
-    text-decoration: none;
-    text-transform: none;
-    color: #000000;
-    background-color: #ffffff;
-  }
-</style>
-<div class="container">
-  <p>Hello Harish, This is running from app engine</p>
-  <p>Kamal was able push code to specific branch</p>
-  <p>it automatically runs tests & get deployed in app engine</p>
-  <p>All he needs to do is make changes , commit &
-  push so that the deployment can be taken care automatically
-  by CI/CD Process :)</p>
-</div>"""
+SAMPLE_RESPONSE = (
+    "Hello Harish, This is running from app engine\n "
+    "Kamal was able push code to specific branch and "
+    "it automatically runs tests & get deployed in app engine \n "
+    "All he needs to do is make changes , commit & "
+    "push so that the deployment can be taken care "
+    "automatically by CI/CD Process :)"
+)
 
 
 @app.get("/")
