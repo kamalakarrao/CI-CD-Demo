640f3a6a91f52651cd813ba5c81a88c6d30504dc
Kamal <kamalakar.sambaraj@gmail.com>
2022-10-22 09:07:46 +0530

    Modified Response --

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 SAMPLE_RESPONSE = (
-    "Hello Harish Gurram, This is running from app engine\n "
+    "Hello Gurram Harish, This is running from app engine\n "
     "Kamal was able push code to specific branch and "
     "it automatically runs tests & get deployed in app engine \n "
-    "All he needs to do is make changes , commit & "
+    "All he needs to do is make changes, commit & "
     "push so that the deployment can be taken care "
     "automatically by CI/CD Process :)"
 )
